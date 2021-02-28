05fe68ce569f070dc0f70a5a044e74c6783042b6
nihalshanp <pnihalshan94@gmail.com>
2021-02-28 23:39:11 +0530

    learned about probs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
+import React, { Component }  from 'react'
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import Func from './Component/func'
+import Class from './Component/class'
+class App extends Component {
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1> Func <br/> ---------</h1>
+      <Func name="Nihal" age="13">
+        <p>I like to play Football</p>
+      </Func>
+      <Func name="shan" age="18">
+        <p>I like to use Laptop</p>
+      </Func>
+      <Func name="p" age="20">
+        <p>I like to use Mobile</p>
+      </Func>
+      <h1> Class <br/> ---------</h1>
+      <Class name="Nihal" age="13"/>
+      <Class name="Shan" age="18"/>
+      <Class name="p" age="20"/>*/
     </div>
   );
+  }
 }
 
 export default App;
