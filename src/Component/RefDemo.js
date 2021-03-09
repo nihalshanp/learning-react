4ba00e3121d5fd2ad8a777ae7789b78aa08698b7
nihalshanp <lnihalshan94@gmail.com>
2021-03-09 18:51:05 +0530

    Learned basics of Refs with callback

--- src/Component/RefDemo.js
+++ src/Component/RefDemo.js
@@ -4,15 +4,22 @@ export default class RefDemo extends Component {
     
     constructor(props){
         super(props)
-        this.inputRef = React.createRef()
+        //this.inputRef = React.createRef()
+        this.cdRef = null
+        this.setCbRef = element => {
+            this.cdRef = element
+        }
     }
     
     componentDidMount(){
-        this.inputRef.current.focus()
+     // this.inputRef.current.focus()
+      if (this.cdRef) {
+          this.cdRef.focus()
+      }
     }
     
     buttonClick = () => {
-        alert(this.inputRef.current.value)
+        alert(this.cdRef.value)
     } 
     
     render() {
@@ -20,8 +27,10 @@ export default class RefDemo extends Component {
         return (
             <div>
                 <h1>Ref Demo</h1>
-                <input type='text' ref={this.inputRef}/>
-                <button onClick={this.buttonClick}>Click me</button>
+               {/* <input type='text' ref={this.inputRef} placeHolder='input 1'/>*/}
+                <input type='text' ref={this.setCbRef} placeHolder='input 2'/>
+                <br/>
+                <button onClick={this.buttonClick} >Click me</button>
             </div>
         )
     }
