55e21007a720ce6bff97e93f5752ab7b23e6f001
Nihal Shan <pnihalshan94@gmail.com>
2021-03-11 22:54:06 +0530

    learned about Renderimg props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, { Component }  from 'react'
 import './App.css';
-import ClickCounter from './Component/ClickCounter'
-import HoverCounter from './Component/HoverCounter'
+import ClickCounterTow from './Component/ClickCounterTow'
+import HoverCounterTow from './Component/HoverCounterTow'
+import User from './Component/User'
+import Counter from './Component/Counter'
 
 class App extends Component {
   render(){
   return (
     <div className="App">
-        <ClickCounter name='Nihal'/>
-        <HoverCounter/>
+    
+        <Counter>
+        {(count, incrementCount) => (
+            <ClickCounterTow count={count} incrementCount={incrementCount}/>
+        )}
+        </Counter>
+        <Counter>
+        {(count, incrementCount) => (
+                <HoverCounterTow count={count} incrementCount={incrementCount}/>
+            )}
+            </Counter>
+        
+       {/* <ClickCounterTow/>
+        <HoverCounterTow/>
+        <User name={(isLoggedin) => isLoggedin ? 'Nihal shan' : 'Guest'}/>*/}
     </div>
   );
   }
 }
 
 export default App;
+                                                                                                                                                                                                                        
